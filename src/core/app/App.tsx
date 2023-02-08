c39e381d866f9dd8ee47f7774e9cf2cbc6ac4f1e
Jimena <jimenanrp2@gmail.com>
2023-02-08 23:57:39 +0100

    ✏️ Fix audit errors and erase css

--- src/core/app/App.tsx
+++ src/core/app/App.tsx
@@ -3,13 +3,12 @@ import { Character } from "../../features/character/page/character";
 import { Header } from "../header/header";
 import "./App.css";
 
-
 function App() {
   return (
     <>
       <Header></Header>
       <main>
-          <Character></Character>
+        <Character></Character>
       </main>
     </>
   );
